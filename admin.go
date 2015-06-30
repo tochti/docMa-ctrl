e16b5fae383d8368a9fff0586d3e136d814451bd
Tim Tochtermann <tim.tochtermann@posteo.de>
2015-06-30 15:03:05 +0200

    first version of import account process data

--- admin.go
+++ admin.go
@@ -5,9 +5,15 @@ import (
   "fmt"
   "flag"
   "path"
+  "time"
   "bufio"
+  "bytes"
+  "errors"
+  "io/ioutil"
   "strings"
+  "net/http"
   "crypto/sha1"
+  "encoding/json"
 
   "github.com/rrawrriw/bebber"
   "gopkg.in/mgo.v2"
@@ -15,36 +21,168 @@ import (
 )
 
 func main() {
+  var docsPath string
+  var accProcessFile string
   adduser := flag.Bool("adduser", false, "Add default user")
+  flag.StringVar(&docsPath, "maketestdata", "", "Reset Docs Collection")
+  flag.StringVar(&accProcessFile, "importaccprocess", "", "Import Account Process from csv")
   flag.Parse()
 
   if *adduser {
-    session, err := mgo.Dial(bebber.GetSettings("BEBBER_DB_SERVER"))
-    if err != nil {
-      fmt.Println(err.Error())
-      return
-    }
-    db := session.DB(bebber.GetSettings("BEBBER_DB_NAME"))
-    users := db.C(bebber.UsersCollection)
-    username, password, dirs := UserMenu()
-    if ExistsUser(username, users) {
-      fmt.Println(username, "already exists!")
-      return
-    }
-    user := bebber.User{Username: username,
-                        Password: password,
-                        Dirs: dirs}
-    err = users.Insert(user)
-    if err != nil {
-      fmt.Println(err.Error())
-      return
-    } else {
-      fmt.Println(username, "save completed!")
-    }
+    AppendUser()
+  }
+
+  if accProcessFile != "" {
+    ImportAccProcessFile(accProcessFile)
+  }
+
+  if docsPath != "" {
+    MakeTestData(docsPath)
   }
 }
 
-func UserMenu() (string, string, map[string]string) {
+func ImportAccProcessFile(accProcessFile string) {
+  urlPrefix := ReadURLPrefix()
+  client := http.DefaultClient
+  token := LoginWithDefaultUser(client)
+
+  accProcessList, err := bebber.ReadAccProcessFile(accProcessFile)
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(2)
+  }
+
+  makeURL := urlPrefix + "/AccProcess"
+  authHeader := http.Header{}
+  authHeader.Add(bebber.TokenHeaderField, token)
+  for _,ap := range accProcessList {
+    accProcessJSON, err := json.Marshal(ap)
+    if err != nil {
+      fmt.Println(err)
+      continue
+    }
+
+    body := strings.NewReader(string(accProcessJSON))
+    request, err := http.NewRequest("POST", makeURL, body)
+    request.Header = authHeader
+    if err != nil {
+      fmt.Println(err.Error())
+      continue
+    }
+    response, err := client.Do(request)
+    if err != nil {
+      fmt.Println(err.Error())
+    }
+    if response.StatusCode != 200 {
+      fmt.Println(response)
+    }
+  }
+
+}
+
+func MakeTestData(docsPath string) {
+  urlPrefix := ReadURLPrefix()
+  client := http.DefaultClient
+  token := LoginWithDefaultUser(client)
+
+  docs, err := ioutil.ReadDir(docsPath)
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(2)
+  }
+
+  makeUrl := urlPrefix + "/Doc"
+  authHeader := http.Header{}
+  authHeader.Add(bebber.TokenHeaderField, token)
+  for i,f := range docs {
+    base := path.Base(f.Name())
+    now := time.Now()
+    requestBody := fmt.Sprintf(`{
+      "Name": "%v",
+      "Infos": {"DateOfScan": "%v", "DateOfReceipt": "%v"},
+      "Labels": ["Neu"]
+    }`, base, now.Format(time.RFC3339), now.Format(time.RFC3339))
+    reader := strings.NewReader(requestBody)
+    makeRequest, err := http.NewRequest("POST", makeUrl, reader)
+    makeRequest.Header = authHeader
+    if err != nil {
+      fmt.Println(err.Error())
+      continue
+    }
+    fmt.Println(i, base)
+    response, err := client.Do(makeRequest)
+    if err != nil {
+      fmt.Println(response)
+      fmt.Println(err.Error())
+      continue
+    }
+    buf := bytes.Buffer{}
+    buf.ReadFrom(response.Body)
+    fmt.Println(buf.String())
+  }
+}
+
+func Login(user, password, urlPrefix string, c *http.Client) (string, error) {
+  loginUrl := urlPrefix + "/Login"
+  loginRequest := fmt.Sprintf(`{"Username":"%v","Password":"%v"}`, user, password)
+  buf := strings.NewReader(loginRequest)
+  response, err := c.Post(loginUrl, "application/json", buf)
+  if err != nil {
+    fmt.Println(err.Error())
+    b := bytes.Buffer{}
+    b.ReadFrom(response.Body)
+    fmt.Println(b.String())
+    return "", errors.New("Cannot Login")
+  }
+
+  token, err := ReadToken(response)
+  if err != nil {
+    return "", err
+  }
+
+  return token, nil
+}
+
+func ReadToken(r *http.Response) (string, error) {
+  cookies := r.Cookies()
+  token := ""
+  for _, v := range cookies {
+    if v.Name == bebber.XSRFCookieName {
+      token = v.Value
+    }
+  }
+  if token != "" {
+    return token, nil
+  } else {
+    return token, errors.New("Cannot found token!")
+  }
+}
+
+func AppendUser() {
+  session, err := mgo.Dial(bebber.GetSettings("BEBBER_DB_SERVER"))
+  if err != nil {
+    fmt.Println(err.Error())
+    return
+  }
+  db := session.DB(bebber.GetSettings("BEBBER_DB_NAME"))
+  users := db.C(bebber.UsersColl)
+  username, password := UserMenu()
+  if ExistsUser(username, users) {
+    fmt.Println(username, "already exists!")
+    return
+  }
+  user := bebber.User{Username: username,
+                      Password: password}
+  err = users.Insert(user)
+  if err != nil {
+    fmt.Println(err.Error())
+    return
+  } else {
+    fmt.Println(username, "save completed!")
+  }
+}
+
+func UserMenu() (string, string) {
   reader := bufio.NewReader(os.Stdin)
   fmt.Print("Username: ")
   username, _ := reader.ReadString('\n')
@@ -53,33 +191,7 @@ func UserMenu() (string, string, map[string]string) {
   passTmp, _ := reader.ReadString('\n')
   passTmp = strings.TrimSpace(passTmp)
   password := fmt.Sprintf("%x", sha1.Sum([]byte(passTmp)))
-  dirs := make(map[string]string)
-  fmt.Println("Direcotries - name:\"/rel/path\" - . to finish")
-  for {
-    fmt.Print("> ")
-    input, _ := reader.ReadString('\n')
-    if input == ".\n" {
-      break
-    }
-
-    tmp := strings.Split(strings.TrimSpace(input), ":")
-    if len(tmp) != 2 {
-      fmt.Println("Wrong input format")
-      continue
-    }
-
-    name := tmp[0]
-    relPath := tmp[1]
-    dir := path.Join(bebber.GetSettings("BEBBER_FILES"), relPath)
-    if _, err := os.Stat(dir); os.IsNotExist(err) {
-      fmt.Println("Cannot find file or directory:", dir)
-      continue
-    } else {
-      dirs[name] = relPath
-    }
-  }
-
-  return username, password, dirs
+  return username, password
 }
 
 func ExistsUser(name string, users *mgo.Collection) bool {
@@ -93,4 +205,22 @@ func ExistsUser(name string, users *mgo.Collection) bool {
   } else {
     return false
   }
+}
+
+func ReadURLPrefix() string {
+  httpHost := bebber.GetSettings("BEBBER_IP")
+  httpPort := bebber.GetSettings("BEBBER_PORT")
+  return fmt.Sprintf("http://%v:%v", httpHost, httpPort)
+}
+
+func LoginWithDefaultUser(client *http.Client) string {
+  user := "a"
+  password := "a"
+  urlPrefix := ReadURLPrefix()
+  token, err :=  Login(user, password, urlPrefix, client)
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(2)
+  }
+  return token
 }
