fd3da09edcb9a17f8d70b25fc9fb012cc752e7ee
Tim Tochtermann <tim.tochtermann@gmail.com>
2016-01-30 10:38:57 +0100

    add new add tables funcs

--- cmds/create-tables.go
+++ cmds/create-tables.go
@@ -1,12 +1,28 @@
 package cmds
 
 import (
-	"github.com/tochti/docMa-handler"
+	"github.com/tochti/docMa-handler/accountingData"
+	"github.com/tochti/docMa-handler/common"
+	"github.com/tochti/docMa-handler/dbVars"
+	"github.com/tochti/docMa-handler/docs"
 	"github.com/tochti/docMa-handler/labels"
+	"github.com/tochti/gin-gum/gumauth"
 )
 
 func CreateTables() error {
-	db := bebber.InitMySQL()
+	db := common.InitMySQL()
 
-	return labels.CreateTables(db)
+	gumauth.AddTables(db)
+	dbVars.AddTables(db)
+	docs.AddTables(db)
+	labels.AddTables(db)
+	accountingData.AddTables(db)
+
+	err := db.CreateTablesIfNotExists()
+	if err != nil {
+		return err
+	}
+
+	return nil
+
 }
